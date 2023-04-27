df2160d4377e1fc981ac3fda43a9661f8520468d
Liz <1762842758@qq.com>
2023-04-27 23:42:49 +0800

    chore: modefy vitepress config 2

--- packages/docs/.vitepress/config.ts
+++ packages/docs/.vitepress/config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from "vitepress";
 export default defineConfig({
   title: "Secret Garden Of QingSong",
   description: "Code World",
-  base: "./secret/",
+  base: "./Home/",
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
     nav: [
