34264941cfd1f86737b4dd25505ea070b2a70a65
Liz <1762842758@qq.com>
2023-04-27 23:17:49 +0800

    fix: fix vitepress  github page style lost

--- packages/docs/.vitepress/config.ts
+++ packages/docs/.vitepress/config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from "vitepress";
 export default defineConfig({
   title: "Secret Garden Of QingSong",
   description: "Code World",
+  base: ".",
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
     logo: "",
